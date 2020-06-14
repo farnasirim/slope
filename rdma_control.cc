2820a5a67d1c5539fad7e46e5a92623b475a4367
Mohammad Nasirifar <farnasirim@gmail.com>
2020-06-14 19:39:19 -0400

    Add structure of the migration flow to rdma control plane

--- rdma_control.cc
+++ rdma_control.cc
@@ -4,6 +4,7 @@
 
 #include "mig.h"
 #include "data.h"
+
 #include "json.hpp"
 
 #include "debug.h"
@@ -19,15 +20,45 @@ RdmaControlPlane::RdmaControlPlane(const std::string& self_name,
   self_name_(self_name),
   cluster_nodes_(cluster_nodes),
   keyvalue_service_(std::move(keyvalue_service)),
-  dataplane_(nullptr) {
-
+  dataplane_(nullptr),
+  ib_context_(ib_device_name_.c_str()),
+  dev_attrs_result_(ibv_query_device(ib_context_.get(), &dev_attrs)),
+  query_port_result_(ibv_query_port(ib_context_.get(),
+        operating_port_num_, &operating_port_attr_)),
+  global_pd_(ib_context_.get()),
+  do_migrate_mr_(global_pd_.get(), &do_migrate_req_, sizeof(do_migrate_req_),
+      do_migrate_mr_flags_),
+  do_migrate_cq_(ib_context_.get(), dev_attrs.max_cqe, static_cast<void *>(NULL),
+                 static_cast<struct ibv_comp_channel *>(NULL), 0)
+{
     if(self_name_ ==
         *min_element(cluster_nodes_.cbegin(), cluster_nodes_.cend())) {
       init_cluster();
     }
 
+    for(auto& peer_name: cluster_nodes_) {
+      if(peer_name == self_name_) {
+        continue;
+      }
+
+      // TODO extrac constants away
+      struct ibv_qp_init_attr qp_init_attr = {};
+      qp_init_attr.send_cq = do_migrate_cq_.get();
+      qp_init_attr.recv_cq = do_migrate_cq_.get();
+      // qp_init_attr.cap.max_send_wr = static_cast<uint33_t>(dev_attrs.max_qp_wr;
+      qp_init_attr.cap.max_send_wr = 1;
+      // qp_init_attr.cap.max_recv_wr = static_cast<uint33_t>(dev_attrs.max_qp_wr);
+      qp_init_attr.cap.max_recv_wr = 1;
+      qp_init_attr.cap.max_send_sge = 1;
+      qp_init_attr.cap.max_recv_sge = 1;
+      // qp_init.cap.max_inline_data = 60;
+      qp_init_attr.qp_type = IBV_QPT_RC;
+    }
+
+
     NodeInfo my_info;
     my_info.node_id = self_name_;
+
     keyvalue_service_->set(self_name_, json(my_info).dump());
     for(auto peer: cluster_nodes_) {
       std::string peer_info;
@@ -43,12 +74,14 @@ void RdmaControlPlane::init_cluster() {
 
 bool RdmaControlPlane::do_migrate(const std::string& dest,
       const std::vector<slope::alloc::memory_chunk>& chunks) {
+
   if(!keyvalue_service_->compare_and_swap(
       migrate_in_progress_cas_name_,
       "0",
       "1")) {
     return false;
   }
+  std::lock_guard<std::mutex> polling_guard(control_plane_polling_lock_);
 
   start_migrate_ping_pong(dest, chunks);
   // Later TODO: prefill
@@ -80,7 +113,6 @@ void RdmaControlPlane::attach_dataplane(slope::data::DataPlane::ptr dataplane) {
 
 void RdmaControlPlane::start_migrate_ping_pong(const std::string& dest,
     const std::vector<slope::alloc::memory_chunk>& chunks) {
-
 }
 
 void RdmaControlPlane::transfer_ownership_ping_pong(const std::string& dest,
--- rdma_control.h
+++ rdma_control.h
@@ -2,11 +2,15 @@
 #define SLOPE_RDMA_CONTROL_H_
 
 #include <memory>
+#include <mutex>
 #include <string>
 
 #include "control.h"
 #include "keyvalue.h"
 #include "data.h"
+#include "ib.h"
+#include "ib_container.h"
+#include "modify_qp.h"
 
 #include "mig.h"
 
@@ -22,9 +26,15 @@ extern "C" {
 struct NodeInfo {
   std::string node_id;
 };
+
+struct DoMigrateRequest {
+
+};
 }
 
+
 class RdmaControlPlane: public ControlPlane {
+
  public:
   using ptr = std::unique_ptr<RdmaControlPlane>;
   virtual bool do_migrate(const std::string& dest,
@@ -57,13 +67,30 @@ class RdmaControlPlane: public ControlPlane {
   static inline const std::string migrate_in_progress_cas_name_ =
     "MIGRATE_IN_PROGRESS_CAS";
 
-
+  // ************ order is important here *********************
+  static inline const std::string ib_device_name_ = "mlx5_1";
   const std::string self_name_;
   const std::vector<std::string> cluster_nodes_;
   slope::keyvalue::KeyValueService::ptr keyvalue_service_;
   slope::data::DataPlane::ptr dataplane_;
 
-  // order is important
+  std::mutex control_plane_polling_lock_;
+
+  IbvDeviceContextByName ib_context_;
+  int dev_attrs_result_;
+  ibv_device_attr dev_attrs;
+  int query_port_result_;
+  static inline const uint8_t operating_port_num_ = 1;
+  struct ibv_port_attr operating_port_attr_;
+  IbvAllocPd global_pd_;
+  static inline const int do_migrate_mr_flags_ =
+      IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE;
+  DoMigrateRequest do_migrate_req_;
+  IbvRegMr do_migrate_mr_;
+  IbvCreateCq do_migrate_cq_;
+  std::unordered_map<std::string, std::unique_ptr<IbvCreateQp>> do_migrate_qps_;
+  // **********************************************************
+
   // std::vector<std::shared_ptr<
 };
 
