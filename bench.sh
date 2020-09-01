db31dd4ecf223b28864d187787baaca6034a53d2
Mohammad Nasirifar <farnasirim@gmail.com>
2020-09-01 02:06:57 -0400

    Improve benchmark script
    - to output the cmdline options
    - to kill already running slope instances

--- bench.sh
+++ bench.sh
@@ -18,18 +18,19 @@ build() {
 zero() {
     MACHINE=0
     ADDR=forwarded-mel-25
-    ssh $ADDR "cd '$SLOPE_DIR' ; mkdir -p '$BENCH_RESULT_DIR/$bench_dir_name' ; ID=$MACHINE SLOPE_CMDLINE_OPTIONS='$SLOPE_CMDLINE_OPTIONS' ./run.sh 1>$BENCH_RESULT_DIR/$bench_dir_name/$MACHINE"
+    ssh $ADDR "killall slope ; cd '$SLOPE_DIR' ; mkdir -p '$BENCH_RESULT_DIR/$bench_dir_name' ; ID=$MACHINE SLOPE_CMDLINE_OPTIONS='$SLOPE_CMDLINE_OPTIONS' ./run.sh 1>$BENCH_RESULT_DIR/$bench_dir_name/$MACHINE"
     scp $ADDR:$SLOPE_DIR/$BENCH_RESULT_DIR/$bench_dir_name/$MACHINE $BENCH_RESULT_DIR/$bench_dir_name
 }
 
 one() {
     MACHINE=1
     ADDR=forwarded-mel-26
-    ssh $ADDR "cd '$SLOPE_DIR' ; scp mel-25:/home/farnasirim/workspace/shared/slope/slope slope2 ; mv slope2 slope;"
+    ssh $ADDR "killall slope ; cd '$SLOPE_DIR' ; scp mel-25:/home/farnasirim/workspace/shared/slope/slope slope2 ; mv slope2 slope;"
     ssh $ADDR "cd '$SLOPE_DIR' ; mkdir -p '$BENCH_RESULT_DIR/$bench_dir_name' ; ID=$MACHINE SLOPE_CMDLINE_OPTIONS='$SLOPE_CMDLINE_OPTIONS' ./run.sh 1>$BENCH_RESULT_DIR/$bench_dir_name/$MACHINE"
     scp $ADDR:$SLOPE_DIR/$BENCH_RESULT_DIR/$bench_dir_name/$MACHINE $BENCH_RESULT_DIR/$bench_dir_name
 }
 
+echo $SLOPE_CMDLINE_OPTIONS
 mkdir -p $BENCH_RESULT_DIR/$bench_dir_name
 build
 zero &
